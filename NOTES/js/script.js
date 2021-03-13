c94f7d7b26aa15a2aa30e49ad2f8cca1865584ea
Parth-K-Pokiya <gadhiyaparth5@gmail.com>
2021-03-13 18:42:47 +0530

    NOTES UPLOADED

---
+++ NOTES/js/script.js
@@ -0,0 +1,19 @@
+
+
+/* =========================================
+                login page
+============================================ */
+//show-hide the password field
+var $;
+$(".toggle-password").click(function () {
+
+  $(this).toggleClass("fa-eye fa-eye-slash");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
+  }
+});
+
+
